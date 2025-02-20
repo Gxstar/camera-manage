ecbcf794c43299d0d12559f2e83f5e971ff40977
gxstar <gengxing123@qq.com>
2025-02-20 16:23:25 +0800

    Revert "初步添加sqladmin"
    This reverts commit 5e11ec419315a5a9baaf3a3c65ef7c92f2a85588.

--- app.py
+++ app.py
@@ -3,10 +3,6 @@ from api.cameras import routes as camera_router
 from api.lenses import routes as lens_router
 from api.brands import routes as brand_router
 from api.users import routes as user_router
-from sqladmin import Admin
-from models.database import engine
-# from admin import admin  # 导入 SQLAdmin 实例
-from admin import CameraAdmin, LensAdmin, UserAdmin, BrandAdmin
 
 app = FastAPI(
     title="Camera Lens Catalog API",
@@ -14,16 +10,12 @@ app = FastAPI(
     version="0.1.0",
 )
 
-admin=Admin(app,engine, title="PhotoGear Admin")
-admin.add_view(BrandAdmin)
-admin.add_view(CameraAdmin)
-admin.add_view(LensAdmin)
-admin.add_view(UserAdmin)
 # 注册路由
 app.include_router(camera_router.router, prefix="/cameras", tags=["cameras"])
 app.include_router(lens_router.router, prefix="/lenses", tags=["lenses"])
 app.include_router(brand_router.router, prefix="/brands", tags=["brands"])
 app.include_router(user_router.router, prefix="/users", tags=["users"])
+
 
 @app.get("/")
 async def read_root():
